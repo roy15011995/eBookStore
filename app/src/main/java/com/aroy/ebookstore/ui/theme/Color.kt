44b9892987f187694427692b64f8a12d72ed73d0
Amit Roy <amitroy314@gmail.com>
2025-11-26 18:00:06 +0530

    eBookStore first commit

---
+++ app/src/main/java/com/aroy/ebookstore/ui/theme/Color.kt
@@ -0,0 +1,32 @@
+package com.aroy.ebookstore.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Mode Colors
+val LightPrimary = Color(0xFF3F51B5) // Indigo
+val LightOnPrimary = Color(0xFFFFFFFF) // White
+val LightSecondary = Color(0xFFFFC107) // Amber
+val LightOnSecondary = Color(0xFF000000) // Black
+val LightBackground = Color(0xFFFFFFFF) // White
+val LightOnBackground = Color(0xFF000000) // Black
+val LightSurface = Color(0xFFF5F5F5) // Light Gray
+val LightOnSurface = Color(0xFF000000) // Black
+val LightPrimaryContainer = Color(0x3F51B533) // Indigo with 20% opacity
+val LightOnPrimaryContainer = Color(0xFF000000)
+val LightSecondaryContainer = Color(0xFFC10733) // Amber with 20% opacity
+val LightOnSecondaryContainer = Color(0xFF000000)
+
+
+// Dark Mode Colors
+val DarkPrimary = Color(0xFF673AB7) // Deep Purple
+val DarkOnPrimary = Color(0xFFFFFFFF) // White
+val DarkSecondary = Color(0xFF009688) // Teal
+val DarkOnSecondary = Color(0xFF000000) // Black
+val DarkBackground = Color(0xFF121212) // Dark Gray
+val DarkOnBackground = Color(0xFFFFFFFF) // White
+val DarkSurface = Color(0xFF1E1E1E) // Charcoal
+val DarkOnSurface = Color(0xFFFFFFFF) // White
+val DarkPrimaryContainer = Color(0x673AB74D) // Deep Purple with 30% opacity
+val DarkOnPrimaryContainer = Color(0xFFFFFFFF)
+val DarkSecondaryContainer = Color(0x0096884D) // Teal with 30% opacity
+val DarkOnSecondaryContainer = Color(0xFFFFFFFF)
